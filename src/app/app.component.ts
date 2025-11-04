c51bb47c701ac4ad03982ee4dae9a6fae4ec0783
timdecool <tim.decool@gmail.com>
2025-11-04 21:30:54 +0100

    error banner component and logic handling fetching errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {map, Observable, take} from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,12 @@ export class AppComponent implements OnInit {
 
   protected olympicService = inject(OlympicService);
   protected isLoading$ !: Observable<boolean>;
+  // protected error$ !: Observable<boolean>;
+  protected errorMessage$ !: Observable<string|null>;
 
   ngOnInit(): void {
     this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+    this.errorMessage$ = this.olympicService.errorMessage
     this.isLoading$ = this.olympicService.olympics.pipe(
       map(olympics => olympics.length === 0)
     );
